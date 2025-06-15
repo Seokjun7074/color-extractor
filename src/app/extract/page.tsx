99749244ae6b1ca7daa6f849e6c6a38110caf595
이석준 <sukjune96@gmail.com>
2025-06-15 23:49:57 +0900

    feat: canvas 클릭 시 색상 추출 로직 구현

--- src/app/extract/page.tsx
+++ src/app/extract/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { rgbToHex } from '@/utils/colorConverter';
 import { useEffect, useRef, useState } from 'react';
 
 export default function ExtractPage() {
   const [image, setImage] = useState<string | undefined>(undefined);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const imageRef = useRef<HTMLInputElement | null>(null);
+  const [color, setColor] = useState<string>('#000');
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
 
   const saveImgFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
@@ -32,7 +34,6 @@ export default function ExtractPage() {
       // 이미지 그리기
       ctx.drawImage(img, 0, 0);
     };
-
     img.src = imageURL;
   };
 
@@ -41,14 +42,40 @@ export default function ExtractPage() {
     imageRef.current.click();
   };
 
+  const pickColor = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const hexColor = getHexColor(canvas, e.clientX, e.clientY);
+    console.log(hexColor);
+    setColor(hexColor);
+  };
+
+  const getHexColor = (canvas: HTMLCanvasElement, clientX: number, clientY: number): string => {
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    if (!ctx) return '';
+
+    const bounding = canvas.getBoundingClientRect();
+    console.log(canvas.width, bounding.width);
+    const scaleX = canvas.width / bounding.width; // 사진의 실제 해상도 / 뷰포트상 길이
+    const scaleY = canvas.height / bounding.height;
+
+    const x = Math.floor((clientX - bounding.left) * scaleX);
+    const y = Math.floor((clientY - bounding.top) * scaleY);
+
+    const pixel = ctx.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+    const hexColor = rgbToHex(data[0], data[1], data[2]);
+    return hexColor;
+  };
+
   useEffect(() => {
     return () => {
       if (image) {
         URL.revokeObjectURL(image);
-        console.log(image);
       }
     };
-  }, []);
+  }, [image]);
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4">
@@ -59,7 +86,10 @@ export default function ExtractPage() {
       >
         사진 추가하기
       </button>
-      <canvas className="h-auto w-sm bg-blue-100" ref={canvasRef}></canvas>
+      <canvas className="h-auto w-sm bg-blue-100" ref={canvasRef} onClick={pickColor}></canvas>
+      <div className="rounded-full border-1 p-2 text-white" style={{ backgroundColor: color }}>
+        색상
+      </div>
     </div>
   );
 }
