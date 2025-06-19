6711dd5e14c9f29d871eb23f87a87d7c757e8582
이석준 <sukjune96@gmail.com>
2025-06-19 16:30:08 +0900

    feat: 캔버스 이미지 등록 시 색상 값 추출

--- src/app/extract/page.tsx
+++ src/app/extract/page.tsx
@@ -7,18 +7,23 @@ import { useEffect } from 'react';
 
 export default function ExtractPage() {
   const { imageURL, saveImgFile } = useImageUpload();
-  const { canvasRef, setImageToCanvas, pickColor } = useCanvasImage();
+  const { canvasRef, setImageToCanvas, getPixelImageData, getContextImageData } = useCanvasImage();
 
   useEffect(() => {
-    if (imageURL) {
-      setImageToCanvas(imageURL);
-    }
+    if (!imageURL) return;
+
+    const initCanvasImage = async (imageURL: string) => {
+      await setImageToCanvas(imageURL);
+      getContextImageData();
+    };
+
+    initCanvasImage(imageURL);
   }, [imageURL]);
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4">
       <ImageUploadButton saveImgFile={saveImgFile} />
-      <canvas className="h-auto w-sm bg-blue-100" ref={canvasRef} onClick={pickColor}></canvas>
+      <canvas className="h-auto w-sm bg-blue-100" ref={canvasRef} onClick={getPixelImageData}></canvas>
     </div>
   );
 }
--- src/domains/extract/hooks/useCanvasImage.ts
+++ src/domains/extract/hooks/useCanvasImage.ts
@@ -1,56 +1,76 @@
-import { rgbToHex } from '@/utils/colorConverter';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useCanvasImage = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
 
-  const setImageToCanvas = (imageURL: string) => {
-    const img = new Image();
-    img.onload = () => {
-      if (!canvasRef.current || !ctxRef.current) return;
-      const canvas = canvasRef.current;
-      const ctx = ctxRef.current;
+  const setImageToCanvas = (imageURL: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = imageURL;
 
-      canvas.width = img.width;
-      canvas.height = img.height;
-
-      ctx.drawImage(img, 0, 0);
-    };
-    img.src = imageURL;
+      img.onload = () => {
+        if (!canvasRef.current || !ctxRef.current) return reject('canvas or ctx is null');
+        const canvas = canvasRef.current;
+        const ctx = ctxRef.current;
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        resolve();
+      };
+      img.onerror = reject;
+    });
   };
-
-  const pickColor = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    if (!canvasRef.current) return;
+  /**
+   * Canvas 전체 영역 픽셀들의 RGBA값을 반환합니다.
+   */
+  const getContextImageData = () => {
+    if (!ctxRef.current || !canvasRef.current) return;
+    const ctx = ctxRef.current;
     const canvas = canvasRef.current;
 
-    const hexColor = getHexColor(canvas, e.clientX, e.clientY);
-    console.log(hexColor);
+    const allPixelData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // console.log(splitPixelRGBA(allPixelData.data));
+    return allPixelData.data;
   };
 
-  const getHexColor = (canvas: HTMLCanvasElement, clientX: number, clientY: number): string => {
-    if (!ctxRef.current) return '';
+  /**
+   * 현재 클릭한 Canvas 좌표의 RGBA값을 반환 합니다.
+   */
+  const getPixelImageData = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!ctxRef.current || !canvasRef.current) return;
     const ctx = ctxRef.current;
+    const canvas = canvasRef.current;
 
-    const pixel = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const bounding = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / bounding.width; // 사진의 실제 해상도 / 뷰포트상 길이
+    const scaleY = canvas.height / bounding.height;
+
+    const x = Math.floor((e.clientX - bounding.left) * scaleX);
+    const y = Math.floor((e.clientY - bounding.top) * scaleY);
+
+    const pixel = ctx.getImageData(x, y, 1, 1);
+
     const data = pixel.data;
-    console.log(splitPixelRGBA(data)[0]);
-    const hexColor = rgbToHex(data[0], data[1], data[2]);
-    return hexColor;
+    console.log('This pixel data', data);
+    return data;
   };
 
-  const splitPixelRGBA = useCallback((rgbaList: Uint8ClampedArray<ArrayBufferLike>) => {
-    const allPixelRGBA = [];
+  /**
+   * RGBA값만 배열에 담아 반환합니다.
+   */
+  const splitPixelRGBA = (rgbaList: Uint8ClampedArray<ArrayBufferLike>) => {
+    const allRGBAList = [];
 
     for (let i = 0; i < rgbaList.length; i += 4) {
       const r = rgbaList[i];
       const g = rgbaList[i + 1];
       const b = rgbaList[i + 2];
       const a = rgbaList[i + 3];
-      allPixelRGBA.push([r, g, b, a]);
+      allRGBAList.push([r, g, b, a]);
     }
-    return allPixelRGBA;
-  }, []);
+    return allRGBAList;
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -59,5 +79,5 @@ export const useCanvasImage = () => {
     }
   }, []);
 
-  return { canvasRef, ctxRef, setImageToCanvas, pickColor };
+  return { canvasRef, ctxRef, setImageToCanvas, getPixelImageData, getContextImageData };
 };
