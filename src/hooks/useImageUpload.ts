471c4a957e53488b9be9f7d88a2bc0ec12b36a81
Seokjun7074 <sukjune96@gmail.com>
2025-07-17 15:22:37 +0900

    feat: 이미지 확장자만 업로드할 수 있도록 변경

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -1,9 +1,21 @@
 import { useEffect, useState } from 'react';
 
 export const useImageUpload = () => {
+  const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg'];
+
   const [imageURL, setImage] = useState<string | undefined>(undefined);
 
+  const isImageFile = (file: File) => {
+    const extension = file.name.split('.').pop()?.toLowerCase();
+    return file.type.startsWith('image/') && extension && imageExtensions.includes(extension);
+  };
+
   const saveImgFile = (file: File) => {
+    if (!isImageFile(file)) {
+      alert('이미지만 업로드 가능합니다.');
+      return;
+    }
+
     cleanUpImageURL(imageURL);
     const url = URL.createObjectURL(file);
     setImage(url);
