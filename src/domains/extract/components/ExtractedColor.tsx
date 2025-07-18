1c87ede8aea6edd89fc7b468d76d079b064cfb00
Seokjun7074 <sukjune96@gmail.com>
2025-07-18 16:28:06 +0900

    feat: 클립보드에 색상 hex코드 복사

--- src/domains/extract/components/ExtractedColor.tsx
+++ src/domains/extract/components/ExtractedColor.tsx
@@ -4,6 +4,11 @@ interface Props {
 }
 
 export default function ExtractedColor({ colors, isLoading }: Props) {
+  const copyHexCode = async (hexCode: string) => {
+    await window.navigator.clipboard.writeText(hexCode);
+    alert(`${hexCode}가 복사되었습니다.`);
+  };
+
   return (
     <div className="flex h-10 w-full justify-between gap-1">
       {isLoading && <div className="h-10 animate-pulse">loading..</div>}
@@ -12,6 +17,7 @@ export default function ExtractedColor({ colors, isLoading }: Props) {
           <div
             className="h-12 w-12 cursor-pointer rounded-md border-1 border-gray-500 select-none"
             style={{ backgroundColor: item }}
+            onClick={() => copyHexCode(item)}
           />
           <div className="pointer-events-none absolute top-full left-1/2 z-50 mt-3 w-max -translate-x-1/2 scale-0 rounded-sm bg-gray-700 px-2 py-1 text-xs text-white opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100">
             {item}
