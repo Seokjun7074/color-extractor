44444c32e79b02de910d27682ac1c4b769f42972
Seokjun7074 <sukjune96@gmail.com>
2025-07-18 15:52:44 +0900

    feat: 마우스 hover 툴팁 제공

--- src/domains/extract/components/ExtractedColor.tsx
+++ src/domains/extract/components/ExtractedColor.tsx
@@ -5,10 +5,18 @@ interface Props {
 
 export default function ExtractedColor({ colors, isLoading }: Props) {
   return (
-    <div className="flex h-10 w-full justify-around gap-1">
+    <div className="flex h-10 w-full justify-between gap-1">
       {isLoading && <div className="h-10 animate-pulse">loading..</div>}
       {colors.map((item) => (
-        <div key={item} className="h-10 w-10 rounded-md border-1 border-gray-600" style={{ backgroundColor: item }} />
+        <div key={item} className="group relative">
+          <div
+            className="h-12 w-12 cursor-pointer rounded-md border-1 border-gray-500 select-none"
+            style={{ backgroundColor: item }}
+          />
+          <div className="pointer-events-none absolute top-full left-1/2 z-50 mt-3 w-max -translate-x-1/2 scale-0 rounded-sm bg-gray-700 px-2 py-1 text-xs text-white opacity-0 transition-all group-hover:scale-100 group-hover:opacity-100">
+            {item}
+          </div>
+        </div>
       ))}
     </div>
   );
